bc96e8e37b8aed69a4e29ea89467b8aad15b3be9
reohinho <reohinho@gmail.com>
2018-12-12 00:20:58 +0800

    -fixing typo -add sopd training -add hands off route by LowConfidentAdapter

--- GUI-Chatterbot.py
+++ GUI-Chatterbot.py
@@ -11,22 +11,30 @@ bot = ChatBot(
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
     input_adapter='chatterbot.input.VariableInputTypeAdapter',
     output_adapter='chatterbot.output.OutputAdapter',
-    database='../database,db',
+    database='../database.db',
     logic_adapters=[
         {
             "import_path": "chatterbot.logic.BestMatch",
             "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
             "response_selection_method": "chatterbot.response_selection.get_first_response"
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.6,
+            'default_response': 'Sorry, I do not undetstand.'
         }
-    ]
+    ],
+    trainer='chatterbot.trainers.ListTrainer'
 )
 
 
-bot.set_trainer(ListTrainer)
+#for files in os.listdir('data/english/'):
+#	data = open('data/english/' + files, 'r').readlines()
+#	bot.train(data)
 
-for files in os.listdir('/Users/jimmychu/Downloads/chatbot/chatterbot-corpus-master/chatterbot_corpus/data/english/'):
-	data = open('/Users/jimmychu/Downloads/chatbot/chatterbot-corpus-master/chatterbot_corpus/data/english/' + files, 'r').readlines()
-	bot.train(data)
+for file in os.listdir('data/sopd/'):
+    data = open('data/sopd/' + file, 'r').readlines()
+    bot.train(data)
 
 
 def command():
